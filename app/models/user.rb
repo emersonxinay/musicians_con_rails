8cafcb0bb0f06d52c88cc556d1b16b44f6e5d709
emerson <xinayespinoza@gmail.com>
2024-01-23 01:37:47 -0300

    fix deploy2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
-  has_one :musician
+  has_one :musician, dependent: :destroy
 end
